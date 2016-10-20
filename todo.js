e3eb5e0929fb85b53ec4f96fd80d5076e79bdde8
tomahawk <tmutunhire@gmail.com>
2016-10-20 15:52:54 +0200

    more deps:

--- todo.js
+++ todo.js
@@ -1,4 +1,6 @@
-riot.tag2('todo', '<h1>{opts.items[0].title}</h1><h2>{blog_title}</h2><ul><li each="{titles}"><h3 class="{completed: bool}">{sequence}</h3><label class="{completed: done}"><input type="checkbox" __checked="{done}" onclick="{shout}"> {name} </label></li><br><form class="" action="index.html" method="post"><input type="text" placeholder="Add another"></form></ul>', '', '', function(opts) {
+riot.tag2('todo', '<h1>{opts.items[0].title}</h1><h2 id="findme">{blog_title}</h2><ul><li each="{titles}"><h3 class="{completed: bool}">{sequence}</h3><label class="{completed: done}"><input type="checkbox" __checked="{done}" onclick="{parent.toggle}"> {name} </label></li><br><form class="form" onsubmit="{add}"><input type="text" placeholder="Add another" name="input" onkeyup="{edit}"></form><label>{text}</label></ul>', '', '', function(opts) {
+var test1;
+
 this.blog_title = "The Coding Writer";
 
 this.titles = [
@@ -13,14 +15,56 @@ this.titles = [
   }, {
     name: "An Incredible Secret",
     sequence: 3
+  }, {
+    name: "RiotJS Rocks!",
+    sequence: 4
   }
 ];
 
-this.bool = true;
+this.bool = false;
 
 this.myvar = "Startups Unlimited";
 
-this.shout = function(e) {
-  return alert("A piece of code");
+this.sequences = 4;
+
+this.toggle = function(e) {
+  var item;
+  item = e.item;
+  item.done = !item.done;
+  return true;
 };
+
+this.getinput = "";
+
+this.edit = function(e) {
+  this.text = e.target.value;
+  return this.getinput = e.target;
+};
+
+this.add = function(e) {
+  this.titles.push({
+    name: this.text,
+    sequence: ++this.sequences
+  });
+  this.text = "";
+  return this.getinput.value = "";
+};
+
+test1 = $("#findme").text();
+
+console.log("test1", test1);
+
+this.on("update", function() {
+  var test2;
+  test2 = $("#findme").text();
+  return console.log("test2", test2);
+});
+
+this.on("mount", function() {
+  var test3;
+  test3 = $("#findme");
+  console.log("test3", test3);
+  $("body").css("text-transform", "uppercase");
+  return $("body").css("font-weight", "100");
+});
 });
--- todo.tag
+++ todo.tag
@@ -1,18 +1,19 @@
 <todo>
   <h1>{ opts.items[0].title }</h1>
-  <h2 >{ blog_title }</h2>
+  <h2 id="findme">{ blog_title }</h2>
   <ul>
     <li each={titles}>
       <h3 class={ completed: bool }>{sequence}</h3>
       <label class={ completed: done }>
-        <input type="checkbox" checked={done} onclick={ shout } /> { name }
+        <input type="checkbox" checked={done} onclick={ parent.toggle } /> { name }
       </label>
     </li>
     <br>
-    <form class="" action="index.html" method="post">
-      <input type="text" placeholder="Add another" />
+    <form class="form" onsubmit={ add }>
+      <input type="text" placeholder="Add another" name="input" onkeyup={ edit } />
     </form>
 
+    <label>{text}</label>
   </ul>
 
   <script type="text/coffee">
@@ -20,12 +21,44 @@
     @titles = [
       {name: "A Secret Book", done: true, sequence: 1},
       {name: "Another Secret Here", done: false, sequence: 2},
-      {name: "An Incredible Secret", sequence: 3}
+      {name: "An Incredible Secret", sequence: 3},
+      {name: "RiotJS Rocks!", sequence: 4}
     ]
-    @bool = true
+    @bool = false
     @myvar = "Startups Unlimited"
+    @sequences = 4
+    @toggle = (e)->
+      # alert "A piece of code"
+      item  = e.item
+      item.done = !item.done
+      true
 
-    @shout = (e)->
-      alert "A piece of code"
+    @getinput = ""
+
+    @edit = (e) ->
+      @text = e.target.value
+      @getinput = e.target
+
+    @add = (e) ->
+      # alert "You want to add #{@text}"
+      @titles.push {name: @text, sequence: ++@sequences}
+      @text = ""
+      @getinput.value = ""
+      # true
+
+    test1 = $("#findme").text()
+    console.log "test1", test1
+
+    this.on "update", ->
+      test2 = $("#findme").text()
+      console.log "test2", test2
+
+    this.on "mount", ->
+      test3 = $("#findme")
+      console.log "test3", test3
+      $("body").css("text-transform", "uppercase")
+      $("body").css("font-weight", "100")
+
+
   </script>
 </todo>
